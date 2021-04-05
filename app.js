b261882affd0e9a2994523c3e303873a64feb336
Morgan Mack <mackleemorgan@gmail.com>
2021-04-05 16:17:43 -0400

    User notes: Secured routes and applied abstract middleware

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable quotes */
-const express = require("express");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const express = require('express');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const app = express();
 app.use(express.json());
 const {
   models: { User, Note },
-} = require("./db");
-const path = require("path");
+} = require('./db');
+const path = require('path');
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.byToken(token);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
-app.post("/api/auth", async (req, res, next) => {
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.post('/api/auth', async (req, res, next) => {
   try {
     const token = await User.authenticate(req.body);
     res.send({ token });
@@ -21,21 +32,22 @@ app.post("/api/auth", async (req, res, next) => {
   }
 });
 
-app.get("/api/auth", async (req, res, next) => {
+app.get('/api/auth', requireToken, async (req, res, next) => {
   try {
-    const user = await User.byToken(req.headers.authorization);
+    // const user = await User.byToken(req.headers.authorization);
 
-    res.send(user);
+    res.send(req.user);
   } catch (ex) {
     next(ex);
   }
 });
 
-app.get("/api/users/:id/notes", async (req, res, next) => {
+app.get('/api/users/:id/notes', requireToken, async (req, res, next) => {
   try {
     const { id } = req.params;
-    const user = await User.byToken(req.headers.authorization);
-    const authId = user.id;
+    // const user = await User.byToken(req.headers.authorization);
+    // const authId = user.id;
+    const authId = req.user.id;
     if (authId != id) return res.sendStatus(404);
     const data = await Note.findAll({
       where: {
