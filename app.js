073a2d8085e5f718cf2ef20ea479b1edfa34b4da
Alan Alban <alan.alban10@gmail.com>
2021-04-05 17:19:04 -0400

    changed id type on notes route

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable quotes */
-const express = require('express');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const app = express();
 app.use(express.json());
 const {
   models: { User, Note },
-} = require('./db');
-const path = require('path');
+} = require("./db");
+const path = require("path");
 
 const requireToken = async (req, res, next) => {
   try {
@@ -21,9 +21,9 @@ const requireToken = async (req, res, next) => {
   }
 };
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-app.post('/api/auth', async (req, res, next) => {
+app.post("/api/auth", async (req, res, next) => {
   try {
     const token = await User.authenticate(req.body);
     res.send({ token });
@@ -32,7 +32,7 @@ app.post('/api/auth', async (req, res, next) => {
   }
 });
 
-app.get('/api/auth', requireToken, async (req, res, next) => {
+app.get("/api/auth", requireToken, async (req, res, next) => {
   try {
     // const user = await User.byToken(req.headers.authorization);
 
@@ -42,13 +42,13 @@ app.get('/api/auth', requireToken, async (req, res, next) => {
   }
 });
 
-app.get('/api/users/:id/notes', requireToken, async (req, res, next) => {
+app.get("/api/users/:id/notes", requireToken, async (req, res, next) => {
   try {
     const { id } = req.params;
     // const user = await User.byToken(req.headers.authorization);
     // const authId = user.id;
     const authId = req.user.id;
-    if (authId != id) return res.sendStatus(404);
+    if (authId !== +id) return res.sendStatus(404);
     const data = await Note.findAll({
       where: {
         userId: id,
